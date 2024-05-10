44d9396051d2da5440672f27290c7b9b332f88dc
hendra-it19 <yusrilihzra@gmail.com>
2024-05-10 21:58:10 +0800

    function, struct, method

---
+++ sesi-3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// method dimiliki oleh suatu tipe data (func yang dimiliki oleh tipe data)
+// func tidak memiliki tipe data sehingga bisa langsung digunakan
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+// func
+func Greet() {
+
+}
+
+// method
+func (p Person) Greet() {
+	fmt.Printf("%+v \n", p)
+}
+
+func main() {
+	var p1 Person = Person{
+		Name: "Hendra",
+		Age:  23,
+	}
+	p1.Greet()
+
+	var p2 Person = Person{
+		Name: "Lusi",
+		Age:  21,
+	}
+	p2.Greet()
+
+}
---
+++ tugas-sesi-3/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Student struct {
+	Id         string
+	Name       string
+	Address    string
+	Occupation string
+	Reason     string
+}
+
+var students = []Student{
+	{
+		Id:         "1",
+		Name:       "Hendra",
+		Address:    "Kendari",
+		Occupation: "Programmer",
+		Reason:     "Belajar",
+	},
+	{
+		Id:         "2",
+		Name:       "Dian",
+		Address:    "Baubau",
+		Occupation: "Mahasiswa",
+		Reason:     "Ingin jadi be developer",
+	},
+	{
+		Id:         "3",
+		Name:       "Indra",
+		Address:    "Baubau",
+		Occupation: "Mahasiswa",
+		Reason:     "Ingin jadi be developer",
+	},
+	{
+		Id:         "4",
+		Name:       "Citra",
+		Address:    "Kolaka",
+		Occupation: "Backend Engineer",
+		Reason:     "Ingin jadi be golang",
+	},
+	{
+		Id:         "5",
+		Name:       "Gita",
+		Address:    "Kendari",
+		Occupation: "Backend Engineer",
+		Reason:     "Ingin menambah bahasa pemrograman baru",
+	},
+}
+
+func findStudent(id string) {
+	for _, value := range students {
+		if value.Id == id {
+			fmt.Printf("Nama %s, Alamat %s, Pekerjaan %s, Alasan %s", value.Name, value.Address, value.Occupation, value.Reason)
+			return
+		}
+	}
+	fmt.Printf("Siswa dengan ID %s, tidak ditemukan !", id)
+}
+
+func main() {
+	var args []string = os.Args
+	// fmt.Printf("%#v \n", args[1])
+	if len(args) < 2 {
+		fmt.Println("Id siswa wajib diisi!")
+		return
+	}
+	findStudent(args[1])
+}
